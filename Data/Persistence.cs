f9ba1b09a5ac7ec5c9ea64cdf9194fe3d1acd92d
Genaro <genaro.toledo@alu.frt.utn.edu.ar>
2025-12-19 14:25:04 -0300

    feat(ej08-data): Agregar persistencia de cuentas bancarias, que puedan ser instanciadas desde las clases necesarias.

---
+++ Data/Persistence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Ej8.Domain.Entities;
+
+namespace Dsw2025Ej8.Data;
+
+public class Persistence
+{
+    List<CuentaBancaria> Cuentas = new List<CuentaBancaria>();
+
+    CuentaBancaria cajaAhorro1 = new CajaDeAhorro("CA-001", 1000000, ["Genaro", "Mariela"]);
+    CuentaBancaria cajaAhorro2 = new CajaDeAhorro("CA-002", 500000, ["Olga", "Juan"]);
+
+    CuentaBancaria cuentaCorriente1 = new CajaDeAhorro("CC-001", 500000, ["Mario", "Pedro"]);
+
+    public void Initialize()
+    { 
+        Cuentas.Add(cajaAhorro1);
+        Cuentas.Add(cajaAhorro2);
+
+        Cuentas.Add(cuentaCorriente1);
+    }
+
+    public List<CuentaBancaria> GetCuentas()
+    {
+        return Cuentas;
+    }
+
+}
