3708ac0b926a7fe2de34fd86b2d69ebfc3b6ae47
Genaro <genaro.toledo@alu.frt.utn.edu.ar>
2025-12-19 14:26:50 -0300

    feat(ej08-domain): Agregar clases para el manejo de excepciones.

---
+++ Domain/Exceptions/SaldoInsuficiente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain.Exceptions;
+
+public class SaldoInsuficiente
+{
+}
