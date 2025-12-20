6e4f756841e70b13469f4f6b69534e55dc3ebe3b
Genaro <genaro.toledo@alu.frt.utn.edu.ar>
2025-12-20 13:10:23 -0300

    feat(ej09): Crear clase Mercancia, que sirve como base para los items que se almacenan en la bodega.

---
+++ Entidades/Mercancia.cs
@@ -0,0 +1,10 @@
+﻿namespace Dsw2025Ej9.Entidades;
+
+public abstract class Mercancia
+{
+    public string Nombre { get; set; }
+    protected Mercancia(string nombre)
+    {
+        Nombre = nombre;
+    }
+}
