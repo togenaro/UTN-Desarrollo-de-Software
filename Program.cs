22ebe7a85aef647d5afe857377e7f25a8c541fbe
Genaro <genaro.toledo@alu.frt.utn.edu.ar>
2025-12-21 10:31:10 -0300

    style(ej09): Agregar comentarios para denotar fragmentos pertinentes a cada punto del enunciado.

--- Program.cs
+++ Program.cs
@@ -1,50 +1,59 @@
-﻿namespace Dsw2025Ej10
+﻿namespace Dsw2025Ej10;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            /* a partir de la lista de productos que se obtiene de producto.crearlistadeejemplo()
-             * resolver:
-             * 1. el primer producto 
-             * 2. el último producto
-             * 3. la suma de precios
-             * 4. el promedio de precios
-             * 5. listar los productos con id mayor a 15
-             * 6. obtener una lista de cada producto con su nombre y el precio en formato moneda, luego mostrar los elementos
-             * 7. el producto con el precio más alto
-             * 8. el producto con el precio más bajo
-             * 9. obtener y mostrar los productos cuyo precio sea mayor al promedio
-             * 10. listar los productos ordenados por descripción de forma descendente
-             * cada punto se debe realizar en un método distinto, en una nueva clase llamada solucion
+        /* a partir de la lista de productos que se obtiene de producto.crearlistadeejemplo()
+         * resolver:
+         * 1. el primer producto 
+         * 2. el último producto
+         * 3. la suma de precios
+         * 4. el promedio de precios
+         * 5. listar los productos con id mayor a 15
+         * 6. obtener una lista de cada producto con su nombre y el precio en formato moneda, luego mostrar los elementos
+         * 7. el producto con el precio más alto
+         * 8. el producto con el precio más bajo
+         * 9. obtener y mostrar los productos cuyo precio sea mayor al promedio
+         * 10. listar los productos ordenados por descripción de forma descendente
+         * cada punto se debe realizar en un método distinto, en una nueva clase llamada solucion
 
-             */
+         */
 
-            var solucion = new Solucion();
+        var solucion = new Solucion();
 
-            Console.WriteLine(solucion.FirstProduct().Descripcion);
+        // 1. El primer producto
+        Console.WriteLine(solucion.FirstProduct().Descripcion);
 
-            Console.WriteLine(solucion.LastProduct().Descripcion);
+        // 2. el último producto
+        Console.WriteLine(solucion.LastProduct().Descripcion);
 
-            Console.WriteLine(solucion.SumOfPrices());
+        // 3. la suma de precios
+        Console.WriteLine(solucion.SumOfPrices());
 
-            Console.WriteLine(solucion.AveragePrice());
+        // 4. el promedio de precios
+        Console.WriteLine(solucion.AveragePrice());
 
-            var productsGT15 = solucion.ListProductsGT15();
-            foreach (var product in productsGT15) Console.WriteLine(product.Id);
+        // 5. listar los productos con id mayor a 15
+        var productsGT15 = solucion.ListProductsGT15();
+        foreach (var product in productsGT15) Console.WriteLine(product.Id);
 
-            var products = solucion.ListProducts();
-            foreach (var product in products) Console.WriteLine(product);
+        // 6. obtener una lista de cada producto con su nombre y el precio en formato moneda, luego mostrar los elementos
+        var products = solucion.ListProducts();
+        foreach (var product in products) Console.WriteLine(product);
 
-            Console.WriteLine($"Producto con mayor precio: {solucion.GetProductWHP().Descripcion} - {solucion.GetProductWHP().Precio}");
+        // 7. el producto con el precio más alto
+        Console.WriteLine($"Producto con mayor precio: {solucion.GetProductWHP().Descripcion} - {solucion.GetProductWHP().Precio}");
 
-            Console.WriteLine($"Producto con menor precio: {solucion.GetProductWLP().Descripcion} - {solucion.GetProductWLP().Precio}");
+        // 8.el producto con el precio más bajo
+        Console.WriteLine($"Producto con menor precio: {solucion.GetProductWLP().Descripcion} - {solucion.GetProductWLP().Precio}");
 
-            var productsHTA = solucion.ListProductsHTA();
-            foreach (var product in productsHTA) Console.WriteLine(product.Id);
+        // 9. obtener y mostrar los productos cuyo precio sea mayor al promedio
+        var productsHTA = solucion.ListProductsHTA();
+        foreach (var product in productsHTA) Console.WriteLine(product.Id);
 
-            var productsOBD = solucion.ListProductOBD();
-            foreach (var product in productsOBD) Console.WriteLine(product.Id);
-        }
+        // 10. listar los productos ordenados por descripción de forma descendente
+        var productsOBD = solucion.ListProductOBD();
+        foreach (var product in productsOBD) Console.WriteLine(product.Id);
     }
 }
